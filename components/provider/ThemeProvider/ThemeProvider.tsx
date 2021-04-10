042f785167dc232c48de20ae9b37b06c5f3b507f
fay <869770665@qq.com>
2021-04-10 20:44:58 +0800

    upd

--- components/provider/ThemeProvider/ThemeProvider.tsx
+++ components/provider/ThemeProvider/ThemeProvider.tsx
@@ -6,12 +6,18 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import React from 'react';
 import kuiTheme from './kuiTheme';
 import {muiTheme} from './theme';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import zhCnLocale from 'date-fns/locale/zh-CN';
+import { zhCN } from '@material-ui/core/locale';
 
 const ThemeProvider = ({children, theme}: any) => {
   return (
-    <MuiThemeProvider theme={{...muiTheme, ...kuiTheme, ...theme}}>
+    <MuiThemeProvider theme={{...muiTheme, ...kuiTheme, ...theme, zhCN}}>
       <CssBaseline />
-      {children}
+      <LocalizationProvider dateAdapter={AdapterDateFns} locale={zhCnLocale}>
+        {children}
+      </LocalizationProvider>
     </MuiThemeProvider>
   )
 };
